dbe346477aa38795e70b841756c81f1cc66731c9
igor <igor.pazynich@yandex.ru>
2017-12-20 16:34:48 +0300

    1

--- homework5-annotations/src/main/java/ru/otus/MyTestFramework/MyTestFramework.java
+++ homework5-annotations/src/main/java/ru/otus/MyTestFramework/MyTestFramework.java
@@ -6,87 +6,13 @@ import ru.otus.MyTestFramework.annotations.Before;
 import ru.otus.MyTestFramework.annotations.Test;
 
 import java.io.File;
-import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
-import java.util.jar.JarEntry;
-import java.util.jar.JarFile;
 
 public class MyTestFramework {
-    static void myPackageRunner (String pkgname){
-        ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
-        // Get a File object for the package
-        File directory = null;
-        String fullPath;
-        String relPath = pkgname.replace('.', '/');
-        System.out.println("ClassDiscovery: Package: " + pkgname + " becomes Path:" + relPath);
-        URL resource = ClassLoader.getSystemClassLoader().getResource(relPath);
-        System.out.println("ClassDiscovery: Resource = " + resource);
-        if (resource == null) {
-            throw new RuntimeException("No resource for " + relPath);
-        }
-        fullPath = resource.getFile();
-        System.out.println("ClassDiscovery: FullPath = " + resource);
-
-        try {
-            directory = new File(resource.toURI());
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(pkgname + " (" + resource + ") does not appear to be a valid URL / URI.  Strange, since we got it from the system...", e);
-        } catch (IllegalArgumentException e) {
-            directory = null;
-        }
-        System.out.println("ClassDiscovery: Directory = " + directory);
-
-        if (directory != null && directory.exists()) {
-            // Get the list of the files contained in the package
-            String[] files = directory.list();
-            for (int i = 0; i < files.length; i++) {
-                // we are only interested in .class files
-                if (files[i].endsWith(".class")) {
-                    // removes the .class extension
-                    String className = pkgname + '.' + files[i].substring(0, files[i].length() - 6);
-                    System.out.println("ClassDiscovery: className = " + className);
-                    try {
-                        classes.add(Class.forName(className));
-                    }
-                    catch (ClassNotFoundException e) {
-                        throw new RuntimeException("ClassNotFoundException loading " + className);
-                    }
-                }
-            }
-        }
-        else {
-            try {
-                String jarPath = fullPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
-                JarFile jarFile = new JarFile(jarPath);
-                Enumeration<JarEntry> entries = jarFile.entries();
-                while(entries.hasMoreElements()) {
-                    JarEntry entry = entries.nextElement();
-                    String entryName = entry.getName();
-                    if(entryName.startsWith(relPath) && entryName.length() > (relPath.length() + "/".length())) {
-                        System.out.println("ClassDiscovery: JarEntry: " + entryName);
-                        String className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
-                        System.out.println("ClassDiscovery: className = " + className);
-                        try {
-                            classes.add(Class.forName(className));
-                        }
-                        catch (ClassNotFoundException e) {
-                            throw new RuntimeException("ClassNotFoundException loading " + className);
-                        }
-                    }
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(pkgname + " (" + directory + ") does not appear to be a valid package", e);
-            }
-        }
-        for (Class<?> type : classes) {
-            myClassRunner(type);
-        }
-    }
     static void myClassRunner(Class<?>... types){
         for (Class<?> type : types) {
             Object object = ReflectionHelper.instantiate(type);
@@ -119,6 +45,48 @@ public class MyTestFramework {
                 }
                 System.out.println();
             }
+        }
+    }
+
+    static void myPackageRunner (String pkgname){
+        ArrayList<Class<?>> classes = new ArrayList<Class<?>>();
+        File directory = null;
+        String fullPath;
+        String relPath = pkgname.replace('.', '/');
+        System.out.println("ClassDiscovery: Package: " + pkgname + " becomes Path:" + relPath);
+        URL resource = ClassLoader.getSystemClassLoader().getResource(relPath);
+        System.out.println("ClassDiscovery: Resource = " + resource);
+        if (resource == null) {
+            throw new RuntimeException("No resource for " + relPath);
+        }
+        fullPath = resource.getFile();
+        System.out.println("ClassDiscovery: FullPath = " + resource);
+
+        try {
+            directory = new File(resource.toURI());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(pkgname + " (" + resource + ") does not appear to be a valid URL / URI.  Strange, since we got it from the system...", e);
+        } catch (IllegalArgumentException e) {
+            directory = null;
+        }
+        System.out.println("ClassDiscovery: Directory = " + directory);
+        if (directory != null && directory.exists()) {
+            String[] files = directory.list();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].endsWith(".class")) {
+                    String className = pkgname + '.' + files[i].substring(0, files[i].length() - 6);
+                    System.out.println("ClassDiscovery: className = " + className);
+                    try {
+                        classes.add(Class.forName(className));
+                    }
+                    catch (ClassNotFoundException e) {
+                        throw new RuntimeException("ClassNotFoundException loading " + className);
+                    }
+                }
+            }
+        }
+        for (Class<?> type : classes) {
+            myClassRunner(type);
         }
     }
 }
