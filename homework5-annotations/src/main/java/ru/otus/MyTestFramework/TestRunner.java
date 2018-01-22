6786020f62e10b0114af7001d4041f322fa16e42
Igor Pazynich <igor.pazynich@yandex.ru>
2018-01-23 00:03:54 +0300

    laga

--- homework5-annotations/src/main/java/ru/otus/MyTestFramework/TestRunner.java
+++ homework5-annotations/src/main/java/ru/otus/MyTestFramework/TestRunner.java
@@ -3,6 +3,12 @@ package ru.otus.MyTestFramework;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class TestRunner {
@@ -16,8 +22,32 @@ public class TestRunner {
         Set<Class<? extends Object>> allClasses = reflections.getSubTypesOf(
                 Object.class
         );
-        allClasses.forEach((cl->System.out.println(cl)));
-        MyTestFramework.myPackageRunner("ru.otus.MyTestFramework");
+        List<URL> paths = new ArrayList<>();
+        for (Class<?> aClass : allClasses) {
+            if (!paths.contains(aClass.getResource(""))) paths.add(aClass.getResource(""));
+        }
+        paths.forEach((p)-> System.out.println(p));
+        String tempPath = paths.get(0).toString().replace("classes","test-classes").replace("file:/","");
+        File file0 = new File(tempPath);
+        for (String s : file0.list()) {
+            System.out.println(s);
+        }
+        URLClassLoader classLoader = null;
+        try {
+            classLoader = URLClassLoader.newInstance(new URL[] { file0.toURI().toURL() });
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Here " + classLoader.getResource(tempPath));
+        try {
+            Class<?> cls = Class.forName("ReflectionHelperTest", true, classLoader);
+            System.out.println(cls.getName());
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        //MyTestFramework.myPackageRunner("ru.otus.MyTestFramework");
 
     }
 }
